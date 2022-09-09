c5203bc8870c025d543c89c40165c9714a3c152a
Oluwadamilare Emmanuel Akerele <folorunshoakerele@gmail.com>
2022-09-09 11:13:53 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,16 +2,7 @@
 
 /**
  * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * `00 01` and `01 00` are considered as the same combination.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 8 times.
- * You are not allowed to use any variable of type `char`.
+ * Description: Use `putchar` to print lowercase and then uppercase alphabet.
  * Return: 0
  */
 
@@ -19,61 +10,35 @@ int main(void)
 
 {
 
-	int i, j;
-
-	int a, b, c, d;
+	char c = 'a';
 
 
-	for (i = 0; i < 100; i++)
+	while (c <= 'z')
 
 	{
 
-		a = i / 10; /* doubles fnum */
+		putchar(c);
 
-		b = i % 10; /* singles fnum */
-
-
-		for (j = 0; j < 100; j++)
-
-		{
-
-			c = j / 10; /* doubles snum */
-
-			d = j % 10; /* singles snum */
-
-
-			if (a < c || (a == c && b < d))
-
-			{
-
-				putchar(a + '0');
-
-				putchar(b + '0');
-
-				putchar(32);
-
-				putchar(c + '0');
-
-				putchar(d + '0');
-
-
-				if (!(a == 9 && b == 8))
-
-				{
-
-					putchar(44);
-
-					putchar(32);
-
-				}
-
-			}
-
-		}
+		c++;
 
 	}
 
-	putchar(10);
+
+	c = 'A';
+
+
+	while (c <= 'Z')
+
+	{
+
+		putchar(c);
+
+		c++;
+
+	}
+
+
+	putchar('\n');
 
 
 	return (0);
