b6ee927d3bd060f1fef97c03b71611b58e98cc64
Oluwadamilare Emmanuel Akerele <folorunshoakerele@gmail.com>
2022-09-09 11:50:06 -0700

    r

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,22 +16,15 @@
  */
 
 int main(void)
-
 {
-
 	int i, j;
-
 	int a, b, c, d;
 
-
 	for (i = 0; i < 100; i++)
-
 	{
-
 		a = i / 10; /* doubles fnum */
 
 		b = i % 10; /* singles fnum */
-
 
 		for (j = 0; j < 100; j++)
 
@@ -64,17 +57,12 @@ int main(void)
 					putchar(44);
 
 					putchar(32);
-
 				}
-
 			}
-
 		}
-
 	}
 
 	putchar(10);
-
 
 	return (0);
 
